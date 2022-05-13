528878f05cec38181cfbf15a9547295bb10b73c7
Felipa Mendez <felipalouise@gmail.com>
2022-05-13 17:37:01 -0500

    conversation controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,17 @@
 class ConversationsController < ApplicationController
+    def index
+        @conversations = Conversation.all
+        render json: @conversations, status: 200
+    end
+
+    def create
+        @conversation = Conversation.create!(conversation_params)
+    end
+
+    private
+
+    def conversation_params
+        params.permit(:content, :user_id)
+    end
+    
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,4 @@
 class Conversation < ApplicationRecord
+
+    
 end
