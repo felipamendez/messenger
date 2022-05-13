192c0aa1891b8fe47789acfdd0a9b17242a35388
Felipa Mendez <felipalouise@gmail.com>
2022-05-13 17:52:31 -0500

    user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,20 @@ class UsersController < ApplicationController
        end
     end
 
-   
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: 422
+    end
+
+   private
+
+   def user_params
+        params.permit(:username, :password)
+   end
 
 
 end
