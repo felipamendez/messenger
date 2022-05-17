1167805949e2e506319a180d72c90c77ea2f79c7
Felipa Mendez <felipalouise@gmail.com>
2022-05-17 10:33:47 -0500

    sessions create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by!(username: params[:username])
-        
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: {errors: "Username or password not valid."}
+        end
     end
 
     private 
