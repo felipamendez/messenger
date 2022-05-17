489811632c2a1289efce969b967f06aff5ce4908
Felipa Mendez <felipalouise@gmail.com>
2022-05-16 20:17:26 -0500

    added show message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+
+    def show
+        conversation = Conversation.find_by(user_id: session[:user_id])
+        if conversation.valid?
+            messages = Message.all.select{|message| message.conversation_id == conversation.id }
+            render json: messages, status: 200
+        else
+            render json: {error: "No conversation to show"}
+        end
+    end
+    
 end
