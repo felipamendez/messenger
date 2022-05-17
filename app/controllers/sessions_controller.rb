f0da44944122edbe58a9b8586b18a657bc6489c1
Felipa Mendez <felipalouise@gmail.com>
2022-05-17 10:31:30 -0500

    record_not_found

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    def create
+        user = User.find_by!(username: params[:username])
+        
+    end
+
+    private 
+
+    def record_not_found
+        render json: {errors:  message.errors.full_messages}, status: :unprocessable_entity
+    end
 end
